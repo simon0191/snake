9c8b9c357e219018b336abb6c2552f5240308532
Simon Soriano <simonsp@uber.com>
2018-09-15 19:51:03 +0200

    Advance snake

--- src/app.rb
+++ src/app.rb
@@ -45,10 +45,27 @@ end
 class Coord < Struct.new(:x, :y)
 end
 
-class Snake < Struct.new(:grid_config, :coords, keyword_init: true)
-  
+class Direction < Struct.new(:coord)
+
+  def self.build(dir)
+    case dir
+    when :north, :NORTH
+      @@north ||= Direction.new(Coord.new(0, -1))
+    when :east, :EAST
+      @@east ||= Direction.new(Coord.new(1, 0))
+    when :south, :SOUTH
+      @@south ||= Direction.new(Coord.new(0, 1))
+    when :west, :WEST
+      @@west ||= Direction.new(Coord.new(-1, 0))
+    end
+  end
+
+end
+
+class Snake < Struct.new(:grid_config, :coords, :direction, keyword_init: true)
+
   def render!
-    coords.each_with_index do |coord, i|
+    @squares = coords.each_with_index.map do |coord, i|
       Square.new(
         x: coord.x * grid_config.square_size,
         y: coord.y * grid_config.square_size,
@@ -56,6 +73,15 @@ class Snake < Struct.new(:grid_config, :coords, keyword_init: true)
         color: i == 0 ? 'blue' : 'green'
       )
     end
+  end
+
+  def advance
+    (1...@squares.size).reverse_each do |idx|
+      @squares[idx].x = @squares[idx - 1].x
+      @squares[idx].y = @squares[idx - 1].y
+    end
+    @squares.first.x += (direction.coord.x * grid_config.square_size)
+    @squares.first.y += (direction.coord.y * grid_config.square_size)
   end
 end
 
@@ -68,8 +94,24 @@ grid_config = Grid::Config.new(
 )
 
 grid = Grid.new(config: grid_config)
-snake = Snake.new(grid_config: grid_config, coords: [Coord.new(2,2), Coord.new(2,3), Coord.new(3,3), Coord.new(3,2), Coord.new(3,1)])
+snake = Snake.new(
+  grid_config: grid_config,
+  coords: [Coord.new(2,2), Coord.new(2,3), Coord.new(3,3), Coord.new(3,2), Coord.new(3,1)],
+  direction: Direction.build(:north))
 
 grid.render!
 snake.render!
+
+def tick
+  snake.advance
+end
+
+last_update = Time.now
+update do
+  if Time.now - last_update > 1
+    last_update = Time.now
+    snake.advance
+  end
+end
+
 show
